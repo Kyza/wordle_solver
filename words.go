77870d629ffaf79661e8cb5686c05b3a79ae9770
Kyza <kyzamizu@proton.me>
2026-02-18 02:47:03 -0500

    build: download word list at build time via go generate
    Removed words.txt from the repo. The list is now fetched from
    tabatkins/wordle-list during 'go generate' and embedded at compile time.

--- words.go
+++ words.go
@@ -1,11 +1,14 @@
 package main
 
 // Word list embedded from words.txt at compile time.
+// Run "go generate" (or use the Makefile) to download the list before building.
 
 import (
 	_ "embed"
 	"strings"
 )
+
+//go:generate curl -sSfL -o words.txt https://raw.githubusercontent.com/tabatkins/wordle-list/main/words
 
 //go:embed words.txt
 var wordsRaw string
